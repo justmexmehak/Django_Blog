474fdcb396f8824f694d3d06d6135d30bde6e48d
justmexmehak <mehaknauman23@gmail.com>
2024-06-14 15:58:49 +0500

    Urls and Views

--- aiproject/blog/views.py
+++ aiproject/blog/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.http import Http404
 
 # Create your views here.
+
+from .models import Post 
+
+def post_list(request):
+    posts = Post.published.all()
+    return render(request, 'blog/post/list.xhtml', {'posts': posts})
+
+def post_detail(request, id):
+    # try:
+    #     post = Post.published.get(id = id)
+    # except Post.DoesNotExist:
+    #     raise Http404("No Post found.")
+    post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
+    return render(request, 'blog/post/detail.xhtml' {'post': post})
